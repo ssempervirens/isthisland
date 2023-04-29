c9e235e979a50047550ff91c344f254a2847cd9c
Camille Fassett <camillefassett@gmail.com>
2023-04-29 10:08:23 -0700

    finish functionality to read in polygons and test whether a point is in the polygons. currently, the land and ocean tests pass but the lake one doesnt

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use anyhow::Result;
-use geo::{Point, Polygon};
+use geo::{Contains, MultiPolygon, Point, Polygon};
 use shapefile::Shape;
 
-/*
-fn get_polygons_shapefile() -> Result<Vec<Polygon<f64>>, anyhow::Error> {
+fn get_polygons_shapefile() -> Result<Vec<MultiPolygon<f64>>, anyhow::Error> {
     // return polygons as a vector and give that to point in polygons function?
     let shapes = shapefile::read("data/ne_10m_land/ne_10m_land.shp")?;
-    let polygons: Vec<Polygon<f64>> = shapes
+    let polygons: Vec<MultiPolygon<f64>> = shapes
         .into_iter()
-        .filter_map(|(shape, record)| match shape {
-            Shape::Polygon(poly) => Some(poly),
+        .filter_map(|(shape, _record)| match shape {
+            Shape::Polygon(polygon) => Some(polygon.into()),
             _ => None,
         })
         .collect();
@@ -17,7 +16,7 @@ fn get_polygons_shapefile() -> Result<Vec<Polygon<f64>>, anyhow::Error> {
     Ok(polygons)
 }
 
-fn point_in_polygons(polygons: Vec<Polygon<f64>>, point: (f64, f64)) -> bool {
+fn point_in_polygons(polygons: Vec<MultiPolygon<f64>>, point: (f64, f64)) -> bool {
     // order is lon lat
     let point = Point::new(point.1, point.0);
     // iterate over each of the ten records as polygons
@@ -28,14 +27,13 @@ fn point_in_polygons(polygons: Vec<Polygon<f64>>, point: (f64, f64)) -> bool {
     }
     false
 }
- */
 
 fn main() -> Result<(), anyhow::Error> {
-    let shapes = shapefile::read("data/ne_10m_land/ne_10m_land.shp")?;
-    for (shape, record) in shapes {
-        let geo_shape = geo_types::Geometry::<f64>::try_from(shape)?;
-        println!("")
-    }
+    // let shapes = shapefile::read("data/ne_10m_land/ne_10m_land.shp")?;
+    // for (shape, record) in shapes {
+    //     let geo_shape = geo_types::Geometry::<f64>::try_from(shape)?;
+    //     println!("")
+    // }
 
     Ok(())
 }
@@ -47,17 +45,24 @@ mod tests {
     #[test]
     fn point_is_land() {
         // fresno, ca should be on land
-        assert_eq!(point_on_land((36.7475, -119.775)), true,)
+        let polygons = get_polygons_shapefile().unwrap();
+        assert_eq!(point_in_polygons(polygons, (36.7475, -119.775)), true,)
     }
 
     #[test]
     fn point_in_lake() {
         // lake tahoe should not be on land
-        assert_eq!(point_on_land((39.096849, -120.032351)), false,)
+        let polygons = get_polygons_shapefile().unwrap();
+        assert_eq!(point_in_polygons(polygons, (39.096849, -120.032351)), false,)
     }
 
+    #[test]
     fn point_in_ocean() {
         // point nemo, in the middle of the ocean, should not be on land
-        assert_eq!(point_on_land((-48.8766667, -123.393333)), false,)
+        let polygons = get_polygons_shapefile().unwrap();
+        assert_eq!(
+            point_in_polygons(polygons, (-48.8766667, -123.393333)),
+            false,
+        )
     }
 }
